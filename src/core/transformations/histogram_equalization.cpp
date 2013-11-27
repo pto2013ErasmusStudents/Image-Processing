87fa4fae52a4336f7e9ab89ec655809760b267c7
pto2013ErasmusStudents <marta.hidalgo@udc.es>
2013-11-27 13:25:14 +0100

    Histogram Task Finished

--- src/core/transformations/histogram_equalization.cpp
+++ src/core/transformations/histogram_equalization.cpp
@@ -1,4 +1,5 @@
 #include "histogram_equalization.h"
+#include <numeric>
 
 #include "../histogram.h"
 
@@ -22,59 +23,67 @@ PNM* HistogramEqualization::transform()
     PNM* newImage = new PNM(width, height, image->format());
 
 	QHash<int, int>::const_iterator i;
-	int minR = 1;
-	int maxR = 0;
+	int j;
+	double probR[256];
+	for (j=0;j<255;++j) {
+		probR[j]=0;
+	}
 	for (i=image->getHistogram()->get(Histogram::RChannel)->constBegin();
 			i!=image->getHistogram()->get(Histogram::RChannel)->constEnd();++i) {
-		if (((double) i.value()/totalPixels)>maxR) {
-			maxR=i.value();
-			maxR/=totalPixels;
-		}
-		if (((double)i.value()/totalPixels)<minR) {
-			minR=i.value();
-			minR/=totalPixels;
-		}
+		double r =i.value();
+		r/=totalPixels;
+		probR[i.key]= r;
 	}
-	int minG = 1;
-	int maxG = 0;
+	double DR[256];
+	DR[0]=0;
+	for (j=0;j<255;++j) {
+		DR[j]=DR[j-1]+probR[j];
+	}
+	double probG[256];
+	for (j=0;j<255;++j) {
+		probG[j]=0;
+	}
 	for (i=image->getHistogram()->get(Histogram::GChannel)->constBegin();
 			i!=image->getHistogram()->get(Histogram::GChannel)->constEnd();++i) {
-		if (((double) i.value()/totalPixels)>maxG) {
-			maxG=i.value();
-			maxG/=totalPixels;
-		}
-		if (((double)i.value()/totalPixels)<minG) {
-			minG=i.value();
-			minG/=totalPixels;
-		}
+		double g =i.value();
+		g/=totalPixels;
+		probG[i.key]= g;
 	}
-	int minB = 1;
-	int maxB = 0;
+	double DG[256];
+	DG[0]=0;
+	for (j=0;j<255;++j) {
+		DG[j]=DG[j-1]+probG[j];
+	}
+	double probB[256];
+	for (j=0;j<255;++j) {
+		probB[j]=0;
+	}
 	for (i=image->getHistogram()->get(Histogram::BChannel)->constBegin();
 			i!=image->getHistogram()->get(Histogram::BChannel)->constEnd();++i) {
-		if (((double) i.value()/totalPixels)>maxB) {
-			maxB=i.value();
-			maxB/=totalPixels;
-		}
-		if (((double)i.value()/totalPixels)<minB) {
-			minB=i.value();
-			minB/=totalPixels;
-		}
+		double b=i.value();
+		b/=totalPixels;
+		probB[i.key]= b;
 	}
-	double minL = 1;
-	double maxL = 0;
+	double DB[256];
+	DB[0]=0;
+	for (j=0;j<255;++j) {
+		DB[j]=DB[j-1]+probB[j];
+	}
+	double probL[256];
+	for (j=0;j<255;++j) {
+		probL[j]=0;
+	}
 	for (i=image->getHistogram()->get(Histogram::LChannel)->constBegin();
 			i!=image->getHistogram()->get(Histogram::LChannel)->constEnd();++i) {
-		if (((double) i.value()/totalPixels)>maxL) {
-			maxL=i.value();
-			maxL/=totalPixels;
-		}
-		if (((double)i.value()/totalPixels)<minL) {
-			minL=i.value();
-			minL/=totalPixels;
-		}
+		double l =i.value();
+		l/=totalPixels;
+		probL[i.key]= l;
 	}
-
+	double DL[256];
+	DL[0]=0;
+	for (j=0;j<255;++j) {
+		DL[j]=DL[j-1]+probL[j];
+	}
 	if (image->format() == QImage::Format_Indexed8)
 	{
 		for (int x=0; x<width; x++) {
@@ -83,13 +92,9 @@ PNM* HistogramEqualization::transform()
 	            QRgb pixel = image->pixel(x,y); // Getting the pixel(x,y) value
 
 				int l = qGray(pixel);    // Get the 0-255 value of the L channel
-				int lValue = image->getHistogram()->get(Histogram::LChannel)->value(l);
-				double p = lValue;
-				p/=totalPixels;
+				int newL = DL[l]*255;
 
-				l = (255/(maxL-minL))*(p-minL);
-
-                newImage->setPixel(x,y, l);
+                newImage->setPixel(x,y, newL);
 			}
 		}
 	}
@@ -100,24 +105,15 @@ PNM* HistogramEqualization::transform()
 			for (int y=0; y<height; y++)
 			{
 				QRgb pixel = image->pixel(x,y); // Getting the pixel(x,y) value
-
 				int r = qRed(pixel);    // Get the 0-255 value of the R channel
-				int rValue = image->getHistogram()->get(Histogram::RChannel)->value(r);
-				double pRed = rValue;
-				pRed/=totalPixels;
-				r = (255/(maxR-minR))*(pRed-minR);
 	            int g = qGreen(pixel);  // Get the 0-255 value of the G channel
-				int gValue = image->getHistogram()->get(Histogram::GChannel)->value(g);
-				double pGreen = gValue;
-				pGreen/=totalPixels;
-				g = (255/(maxG-minG))*(pGreen-minR);
 	            int b = qBlue(pixel);   // Get the 0-255 value of the B channel
-				int bValue = image->getHistogram()->get(Histogram::BChannel)->value(b);
-				double pBlue = bValue;
-				pBlue/=totalPixels;
-				b = (255/(maxB-minB))*(pBlue-minB);
 
-				QColor newPixel = QColor(r,g,b);
+				int newR =DR[r]*255;
+				int newG =DR[g]*255;
+				int newB =DR[b]*255;
+
+				QColor newPixel = QColor(newR,newG,newB);
 				newImage->setPixel(x,y, newPixel.rgb());
 
 			}
